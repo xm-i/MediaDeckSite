ce06a5eb8be85550ed69129bb3809d55f3d442d0
xm-i <testakina@gmail.com>
2026-05-17 21:02:23 +0900

    ダウンロードリンク先をGithubのReleasesに変更

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -83,7 +83,7 @@ onUnmounted(() => {
         数万件のメディアも瞬時に検索。画像や動画、PDF、アーカイブをWindowsで軽快に管理。
       </p>
       <div class="hero-actions animate-fade-in delay-300">
-        <a href="https://github.com/xm-i/MediaDeck" class="btn btn-primary btn-lg">
+        <a href="https://github.com/xm-i/MediaDeck/releases" class="btn btn-primary btn-lg">
           ダウンロード
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
         </a>
